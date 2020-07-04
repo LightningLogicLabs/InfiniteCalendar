7803ea73e533cf8e4bc098627946cfbf77c47067
Joshua Rodriguez <lightninglogiclabs@gmail.com>
2020-07-04 13:17:42 -0400

    Switched to GridLayout

--- app/src/main/java/com/example/infinitecalendar/MainActivity.kt
+++ app/src/main/java/com/example/infinitecalendar/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.infinitecalendar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
+@ExperimentalStdlibApi
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/infinitecalendar/infinite_calendar/InfiniteCalendarView.kt
+++ app/src/main/java/com/example/infinitecalendar/infinite_calendar/InfiniteCalendarView.kt
@@ -3,260 +3,233 @@ package com.example.infinitecalendar.infinite_calendar
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.PagerSnapHelper
+import android.widget.FrameLayout
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.infinitecalendar.R
-import kotlinx.android.synthetic.main.app_calendar_layout.view.*
-import kotlinx.android.synthetic.main.grid_view_calendar_layout.view.*
+import kotlinx.android.synthetic.main.infinite_calendar_layout.view.*
+import kotlinx.android.synthetic.main.calendar_day_item.view.*
+import kotlinx.android.synthetic.main.calendar_month_item.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayDeque
 
 @ExperimentalStdlibApi
-class InfiniteCalendarView(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+class InfiniteCalendarView(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
+
+    interface CalendarItem
 
     interface CalendarDayPressedListener {
         fun itemPressed(day: Date)
     }
 
     companion object {
-        private const val MONTHLY_VIEW_DAY_COUNT = 42
+        private const val MONTHLY_VIEW_DAY_COUNT = 43
         private const val DATE_FORMAT = "MMMM,dd,yyyy"
         private const val COMMA = ","
+        private const val DATE_VIEW_TYPE = 0
+        private const val MONTH_VIEW_TYPE = 1
     }
 
-    data class CalendarMonth(
-        val month: String,
-        val days: List<Date>,
-        val time: Date
-    )
+    private data class DayItem(
+        val date: Date = Date(),
+        val calendarTime: Date = Date()
+    ): CalendarItem
+
+    private data class MonthItem(
+        val month: String = ""
+    ): CalendarItem
 
     private var calendarDayPressed: CalendarDayPressedListener? = null
-    private var adapter: AppCalendarAdapter
-    private var layoutManager: LinearLayoutManager
-    private val pagerSnapHelper: PagerSnapHelper = PagerSnapHelper()
-    private val layout = View.inflate(context, R.layout.app_calendar_layout, this)
-    private val scrollLeftButton: ImageView = layout.scrollLeftButton
-    private val scrollRightButton: ImageView = layout.scrollRightButton
+    private var adapter: InfiniteCalendarAdapter
+    private var layoutManager: GridLayoutManager
+    private val layout = View.inflate(context, R.layout.infinite_calendar_layout, this)
     private val recyclerView: RecyclerView = layout.recyclerView
-    private var currentPosition: Int = 0
     private val calendarNext = Calendar.getInstance()
     private val calendarPrev = Calendar.getInstance()
 
     init {
-        adapter = AppCalendarAdapter(generateInitialMonths(), null)
+        adapter = InfiniteCalendarAdapter(generateInitialMonths(), object : CalendarDayPressedListener {
+            override fun itemPressed(day: Date) {
+                Log.d("DATE", day.toString())
+            }
+        })
         recyclerView.adapter = adapter
-        layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+        layoutManager = GridLayoutManager(context, 7)
         recyclerView.layoutManager = layoutManager
         recyclerView.layoutManager?.scrollToPosition(adapter.itemCount / 2)
-        pagerSnapHelper.attachToRecyclerView(recyclerView)
-        setOnScrollListeners()
-        setClickListeners()
+        recyclerView.recycledViewPool.setMaxRecycledViews(DATE_VIEW_TYPE, 90)
+        recyclerView.recycledViewPool.setMaxRecycledViews(MONTH_VIEW_TYPE, 12)
+        recyclerView.addOnScrollListener(GenerateDayItemListener())
+        layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return if (position % MONTHLY_VIEW_DAY_COUNT == 0) {
+                    7
+                } else 1
+            }
+        }
     }
 
-    private fun generateInitialMonths(): List<CalendarMonth> {
-        val currentMonth = CalendarMonth(
-            generateNameAndYearOfMonth(calendarNext),
-            generateDaysInMonth(calendarNext),
-            calendarNext.time
-        )
+    private fun generateInitialMonths(): List<CalendarItem> {
+        val currentMonth = generateDaysInMonth(calendarNext)
 
         calendarPrev.add(Calendar.MONTH, -1)
 
-        val previousMonth = CalendarMonth(
-            generateNameAndYearOfMonth(calendarPrev),
-            generateDaysInMonth(calendarPrev),
-            calendarPrev.time
-        )
+        val previousMonth = generateDaysInMonth(calendarPrev)
 
         calendarNext.add(Calendar.MONTH, 1)
 
-        val nextMonth = CalendarMonth(
-            generateNameAndYearOfMonth(calendarNext),
-            generateDaysInMonth(calendarNext),
-            calendarNext.time
-        )
+        val nextMonth = generateDaysInMonth(calendarNext)
 
-        return listOf(previousMonth, currentMonth, nextMonth)
+        currentMonth.addAll(nextMonth)
+
+        previousMonth.addAll(currentMonth)
+
+        return previousMonth
     }
 
-    private fun generateDaysInMonth(calendar: Calendar): List<Date> {
-        val cells = mutableListOf<Date>()
+    private fun generateDaysInMonth(calendar: Calendar): MutableList<CalendarItem> {
+        val cells = mutableListOf<CalendarItem>()
         val calendarClone = calendar.clone() as Calendar
+        cells.add(MonthItem(getNameAndYearOfMonth(calendarClone)))
         calendarClone.set(Calendar.DAY_OF_MONTH, 1)
         val monthBeginningCell = calendarClone.get(Calendar.DAY_OF_WEEK) - 1
         calendarClone.add(Calendar.DAY_OF_MONTH, -monthBeginningCell)
         while (cells.size < MONTHLY_VIEW_DAY_COUNT) {
-            cells.add(calendarClone.time)
+            val dayItem = DayItem(
+                calendarClone.time,
+                calendar.time
+            )
+            cells.add(dayItem)
             calendarClone.add(Calendar.DAY_OF_MONTH, 1)
         }
         return cells
     }
 
-    private fun generateNameAndYearOfMonth(calendar: Calendar) : String {
+    private fun getNameAndYearOfMonth(calendar: Calendar) : String {
         val sdf = SimpleDateFormat(DATE_FORMAT, Locale.ENGLISH)
         val date = sdf.format(calendar.time).split(COMMA)
         return "${date[0]} ${date[2]}"
     }
 
-    private fun setOnScrollListeners() {
-        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                currentPosition = layoutManager.findFirstVisibleItemPosition()
-                layout.date.text = adapter.getItems()[currentPosition].month
-                when {
-                    adapter.itemCount == layoutManager.findLastVisibleItemPosition().inc() -> {
-                        recyclerView.post {
-                            calendarNext.add(Calendar.MONTH, 1)
-                            adapter.addItemToFront(CalendarMonth(
-                                generateNameAndYearOfMonth(calendarNext),
-                                generateDaysInMonth(calendarNext),
-                                calendarNext.time
-                            ))
-                        }
+    private inner class GenerateDayItemListener : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            when {
+                adapter.itemCount == layoutManager.findLastVisibleItemPosition().inc() -> {
+                    recyclerView.post {
+                        calendarNext.add(Calendar.MONTH, 1)
+                        adapter.addNextCalendarItems(generateDaysInMonth(calendarNext))
                     }
-                    layoutManager.findFirstVisibleItemPosition() == 0 -> {
-                        recyclerView.post {
-                            calendarPrev.add(Calendar.MONTH, -1)
-                            adapter.addItemToBack(CalendarMonth(
-                                generateNameAndYearOfMonth(calendarPrev),
-                                generateDaysInMonth(calendarPrev),
-                                calendarPrev.time
-                            ))
-                        }
+                }
+                layoutManager.findFirstVisibleItemPosition() == 0 -> {
+                    recyclerView.post {
+                        calendarPrev.add(Calendar.MONTH, -1)
+                        adapter.addPrevDayItems(generateDaysInMonth(calendarPrev))
                     }
                 }
             }
-        })
-    }
-
-    private fun setClickListeners() {
-        scrollLeftButton.setOnClickListener {
-            if (currentPosition > 0) {
-                recyclerView.smoothScrollToPosition(currentPosition - 1)
-            }
-        }
-        scrollRightButton.setOnClickListener {
-            if (currentPosition < adapter.itemCount) {
-                recyclerView.smoothScrollToPosition(currentPosition + 1)
-            }
         }
     }
 
-    @ExperimentalStdlibApi
-    private class AppCalendarAdapter(
-        initialMonths: List<CalendarMonth>,
+    private inner class InfiniteCalendarAdapter(
+        dayItems: List<CalendarItem>,
         private val calendarDayPressed: CalendarDayPressedListener?
     ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-        @ExperimentalStdlibApi
-        private val deque: ArrayDeque<CalendarMonth> = ArrayDeque()
+        val days: MutableList<CalendarItem> = mutableListOf()
 
         init {
-            deque.addAll(initialMonths)
+            days.addAll(dayItems)
+        }
+
+        override fun getItemViewType(position: Int): Int {
+            return if (position % MONTHLY_VIEW_DAY_COUNT == 0) {
+                MONTH_VIEW_TYPE
+            } else {
+                DATE_VIEW_TYPE
+            }
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            return CalendarGridViewHolder(
-                LayoutInflater.from(parent.context).inflate(
-                    R.layout.grid_view_calendar_layout,
-                    parent,
-                    false
+            return when (viewType) {
+                DATE_VIEW_TYPE -> DayItemViewHolder(
+                    LayoutInflater.from(parent.context).inflate(
+                        R.layout.calendar_day_item,
+                        parent,
+                        false
+                    )
                 )
-            )
+               else -> MonthItemViewHolder(
+                    LayoutInflater.from(parent.context).inflate(
+                        R.layout.calendar_month_item,
+                        parent,
+                        false
+                    )
+                )
+            }
         }
 
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            holder as CalendarGridViewHolder
-            holder.bindView(deque[position], calendarDayPressed)
-        }
-
-        override fun getItemCount(): Int = deque.size
-
-        fun getItems(): ArrayDeque<CalendarMonth> = deque
-
-        fun addItemToBack(item: CalendarMonth) {
-            deque.addFirst(item)
-            notifyItemRangeInserted(0, 1)
-        }
-
-        fun addItemToFront(item: CalendarMonth) {
-            deque.addLast(item)
-            notifyItemRangeInserted(deque.size, 1)
-        }
-    }
-
-    @ExperimentalStdlibApi
-    private class CalendarGridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        private val gridView: GridView = itemView.calendar_grid
-        private lateinit var adapter: CalendarGridViewAdapter
-
-        fun bindView(
-            calendarMonth: CalendarMonth,
-            calendarDayPressed: CalendarDayPressedListener?
-        ) {
-            adapter = CalendarGridViewAdapter(
-                itemView.context,
-                calendarMonth.days.toMutableList(),
-                calendarMonth.time
-            )
-            gridView.adapter = adapter
-            gridView.setOnItemClickListener { _, _, position, _ ->
-                val date = gridView.getItemAtPosition(position) as Date
-                calendarDayPressed?.itemPressed(date)
+            if (position % MONTHLY_VIEW_DAY_COUNT == 0) {
+                val monthItem = days[position] as MonthItem
+                (holder as MonthItemViewHolder).bindView(monthItem.month)
+            } else {
+                val dayItem = days[position] as DayItem
+                (holder as DayItemViewHolder).bindView(dayItem.calendarTime, dayItem.date, calendarDayPressed)
             }
         }
+
+        override fun getItemCount(): Int = days.size
+
+        fun addPrevDayItems(prevDayItems: List<CalendarItem>) {
+            days.addAll(0, prevDayItems)
+            notifyItemRangeInserted(0, prevDayItems.size)
+        }
+
+        fun addNextCalendarItems(nextDayItems: List<CalendarItem>) {
+            days.addAll(nextDayItems)
+            notifyItemRangeInserted(days.size - nextDayItems.size, nextDayItems.size)
+        }
     }
 
-    private class CalendarGridViewAdapter(
-        context: Context,
-        days: MutableList<Date>,
-        private val time: Date
-    ) : ArrayAdapter<Date>(context, R.layout.grid_view_calendar_day_item, days) {
+    private inner class DayItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        private val inflater: LayoutInflater = LayoutInflater.from(context)
-
-        override fun getView(position: Int, view: View?, parent: ViewGroup): View {
+        fun bindView(calendarTime: Date, date: Date, calendarDayPressed: CalendarDayPressedListener?) {
             val today = Date()
-            val date = getItem(position)
-            var itemView = view
+            itemView.calendarDay.text = date.date.toString()
+            itemView.calendarDay.setTextColor(Color.parseColor("#FFFFFF"))
 
-            if (view == null) {
-                itemView = inflater.inflate(R.layout.grid_view_calendar_day_item, parent, false)
-            }
-
-            itemView as LinearLayout
-
-            val calendarDayText = itemView.findViewById<TextView>(R.id.calendar_day_text)
-
-            calendarDayText.text = date?.date.toString()
-            calendarDayText.setTextColor(Color.parseColor("#FFFFFF"))
-
-            if (isDayNotInMonth(date)) {
-                calendarDayText.setTextColor(Color.parseColor("#bdbdbd"))
-                calendarDayText.isClickable = false
+            if (isDayNotInMonth(date, calendarTime)) {
+                itemView.calendarDay.setTextColor(Color.parseColor("#838383"))
             }
 
             if (isToday(today, date)) {
-                calendarDayText.setTextColor(Color.parseColor("#42a5f5"))
+                itemView.calendarDay.setTextColor(Color.parseColor("#428FCB"))
             }
 
-            return itemView
+            itemView.setOnClickListener {
+                calendarDayPressed?.itemPressed(date)
+            }
         }
 
-        private fun isDayNotInMonth(date: Date?): Boolean {
-            return date?.month != time.month || time.year != date.year
+        private fun isDayNotInMonth(date: Date?, calendarTime: Date): Boolean {
+            return date?.month != calendarTime.month || calendarTime.year != date.year
         }
 
         private fun isToday(today: Date, date: Date?): Boolean {
-            return today.date == date?.date && today.month == date.month
+            return today.date == date?.date && today.month == date.month && today.year == date.year
+        }
+    }
+
+    private inner class MonthItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(month: String) {
+            itemView.monthName.text = month
         }
     }
 }
